7295712ff25279e164c59267e13f1575c0a89374
wxxxxk <leejaewook17@naver.com>
2026-08-11 13:32:36 +0900

    fix: resolve workspace path relative to script location

--- setup_local.sh
+++ setup_local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Setup unitree ros2 simulation environment"
 source /opt/ros/humble/setup.bash
-source $HOME/unitree_ros2/cyclonedds_ws/install/setup.bash
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/cyclonedds_ws/install/setup.bash"
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
                             <NetworkInterface name="lo" priority="default" multicast="default" />
